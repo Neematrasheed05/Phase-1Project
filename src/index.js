fbb2e21aa4fbe95cdb28e0fc90a10c34333d5330
Neematrashid <nymatrasheed05@gmail.com>
2023-10-12 11:57:14 +0300

    updated css, html started js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const makeupCard = document.querySelector('.makeup-card');
+  const productAttributes = document.getElementById('product-attributes');
+  const searchInput = document.getElementById('search-input');
+  const searchBtn = document.getElementById('search-btn');
+  const prevPageBtn = document.getElementById('prev-page');
+  const nextPageBtn = document.getElementById('next-page');
+  const paginationContainer = document.getElementById('pagination-container');
+
+ 
